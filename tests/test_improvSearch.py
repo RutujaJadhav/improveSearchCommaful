433f5abc1c9fc5d1bb17e2330c0dda70cdcfcc37
Rutuja <rutuja@uw.edu>
2020-09-22 01:47:25 +0530

    intial tests

---
+++ tests/test_improvSearch.py
@@ -0,0 +1,11 @@
+import numpy as np
+import numpy.testing as npt
+import improvSearch
+
+def test_improvSearch_smoke(self):
+    obj = improvSearch.imporvS()
+
+def test_improvSearch_fizz(self):
+    obj = improvSearch.improvS()
+    res = obj.fizz()
+    np.assert_equal(res,"buzz")
