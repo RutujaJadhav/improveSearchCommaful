8eee63c79b6fb28f15d278a1afd3e2f1d3dd07cb
Rutuja <rutuja@uw.edu>
2020-09-22 01:28:08 +0530

    directory initialization, setup

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name = "improvSearch",
+    author = "Rutuja Jadhav",
+    author_email = "rutuja.jadhav543@gmail.com",
+    url = "https://github.com/RutujaJadhav/improveSearchCommaful"
+    packages = ["improvSearch"])
