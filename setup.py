0bed503baeccda0cd923dd7b429587b1a64d2482
Rutuja <rutuja@uw.edu>
2020-10-31 22:18:41 -0700

    scripts

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(name = "improvSearch",
     author = "Rutuja Jadhav",
     author_email = "rutuja.jadhav543@gmail.com",
-    url = "https://github.com/RutujaJadhav/improveSearchCommaful"
+    url = "https://github.com/RutujaJadhav/improveSearchCommaful",
     packages = ["improvSearch"])
